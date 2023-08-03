b1ff08519cb010715301ca7a35aa6a99250ea588
Ajay <er.rokade@gmail.com>
2023-08-03 16:58:45 +0530

    Oppsconcept Actionspackage actions.java

---
+++ OOPs concepet/src/Actions/Actions.java
@@ -0,0 +1,20 @@
+package Actions;
+
+public class Actions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello i am ajay rokade");
+		System.out.print("llhiwe djfhew");
+
+		
+		
+		
+		
+		
+
+	}
+
+}
