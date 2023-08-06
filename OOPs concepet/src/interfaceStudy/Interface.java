886379eb34ba704a3be7fb66402f62c93bc0984b
Ajay <er.rokade@gmail.com>
2023-08-06 23:01:32 +0530

    now interface add

---
+++ OOPs concepet/src/interfaceStudy/Interface.java
@@ -0,0 +1,11 @@
+package interfaceStudy;
+
+public interface Interface {
+	
+	int a=10;     // static and  final
+	int b=20;
+	
+	void m1();    //public and abstract
+	void m2();
+	
+}
---
+++ OOPs concepet/src/interfaceStudy/Interface1.java
@@ -0,0 +1,32 @@
+package interfaceStudy;
+
+public class Interface1 implements Interface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+	Interface1 i=new Interface1();	
+	i.m1();
+	i.m2();
+	i.m3();
+
+	}
+	
+	public void m1()
+	{
+		System.out.println("This is m1 method");
+	}
+	
+	public void m2()
+	{
+		System.out.println("This is m2 method");
+	}
+	public void m3()
+	{
+		System.out.println("This is m3 method");
+	}
+
+}
